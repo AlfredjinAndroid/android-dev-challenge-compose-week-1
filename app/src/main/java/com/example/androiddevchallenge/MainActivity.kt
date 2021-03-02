3a98820e87ddaec5eea048f5674d9212eb3fd488
alfred <alfredjin@163.com>
2021-03-02 17:53:55 +0800

    commit code

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,16 +18,35 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavOptionsBuilder
+import androidx.navigation.NavType
+import androidx.navigation.compose.*
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        loadData()
         setContent {
             MyTheme {
                 MyApp()
@@ -36,26 +55,221 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
+private val petList = mutableListOf<Pet>()
+
+//private var adoptList = mutableStateOf(mutableListOf<Int>())
+//private val adoptList by remember { mutableStateOf(listOf<Int>()) }
+
+fun loadData() {
+    val imgList = listOf(R.mipmap.pic_1, R.mipmap.pic_2, R.mipmap.pic_3, R.mipmap.pic_4)
+    repeat(10) { index ->
+        petList += Pet(
+            id = index,
+            name = "Dog$index",
+            img = imgList[index % 4],
+            desc = "Wow!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+        )
+    }
+}
+
 // Start building your app here!
 @Composable
 fun MyApp() {
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+        val controller = rememberNavController()
+        val petViewModel: PetViewModel = viewModel()
+        NavHost(
+            navController = controller,
+            startDestination = "list",
+            builder = {
+                composable(MyNav.List.router) {
+                    ListPage(petList, controller)
+                }
+                composable(
+                    MyNav.Details.Router.router,
+                    arguments = listOf(navArgument("pet_id") { type = NavType.IntType })
+                ) { entry ->
+                    val petIndex = entry.arguments?.getInt("pet_id") ?: 0
+                    PetDetailsPage(petIndex, controller, petViewModel)
+                }
+            })
+
+    }
+}
+//
+//@Preview("Light Theme", widthDp = 360, heightDp = 640)
+//@Composable
+//fun LightPreview() {
+//    MyTheme {
+//        MyApp()
+//    }
+//}
+//
+//@Preview("Dark Theme", widthDp = 360, heightDp = 640)
+//@Composable
+//fun DarkPreview() {
+//    MyTheme(darkTheme = true) {
+//        MyApp()
+//    }
+//}
+
+@Composable
+fun ListPage(petList: List<Pet>, navController: NavController) {
+    LazyColumn(content = {
+        items(petList) { pet ->
+            ItemPet(pet, navController)
+        }
+    })
+}
+
+@Composable
+fun ItemPet(pet: Pet, navController: NavController) {
+    val pIndex = petList.indexOf(pet)
+    val index = if (pIndex < 0) 0 else pIndex
+    Card(
+        Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clickable {
+                navController.navigateTo(MyNav.Details.Navigation(index))
+            }
+    ) {
+        Row {
+            Image(
+                painter = painterResource(id = pet.img),
+                contentDescription = pet.name,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.size(128.dp),
+            )
+            Box(
+                modifier = Modifier
+                    .height(128.dp)
+                    .padding(16.dp)
+            ) {
+                ItemRight(pet)
+            }
+        }
     }
 }
 
-@Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
-fun LightPreview() {
-    MyTheme {
-        MyApp()
+fun ItemRight(pet: Pet) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = pet.name,
+            style = TextStyle(
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
+            ),
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1.0f, true)
+        )
+        Text(
+            text = pet.desc,
+            style = TextStyle(
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+            ),
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1.0f, true),
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+        )
     }
 }
 
-@Preview("Dark Theme", widthDp = 360, heightDp = 640)
 @Composable
-fun DarkPreview() {
-    MyTheme(darkTheme = true) {
-        MyApp()
+fun PetDetailsPage(id: Int, navController: NavController, petVm: PetViewModel = viewModel()) {
+    val pet = petList.find { f -> f.id == id } ?: return
+    Column {
+        Box(
+            modifier = Modifier
+                .size(48.dp)
+                .clickable { navController.popBackStack() },
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_baseline_arrow_back_24),
+                contentDescription = "Go Back",
+            )
+        }
+        Image(
+            painter = painterResource(id = pet.img), contentDescription = pet.name,
+            Modifier
+                .fillMaxWidth()
+                .height(300.dp)
+                .padding(16.dp),
+            contentScale = ContentScale.Crop
+        )
+
+        val isAdopted = petVm.adoptedList.any { a -> a == pet.id }
+
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(onClick = {
+                petVm.adp(pet = pet)
+            }) {
+                if (isAdopted) {
+                    Text(text = "Adopted")
+                } else {
+                    Text(text = "Adopt")
+                }
+            }
+        }
+
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = pet.name)
+        }
+
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Text(text = pet.desc.repeat(30))
+        }
+
+
+    }
+}
+
+data class Pet(
+    val id: Int,
+    val name: String,
+    val img: Int,
+    val desc: String,
+)
+
+sealed class MyNav(val router: String) {
+    object List : MyNav(router = "list")
+    sealed class Details(router: String) : MyNav(router) {
+        object Router : Details("details/{pet_id}")
+        class Navigation(id: Int) : Details("details/$id")
+    }
+}
+
+fun NavController.navigateTo(nav: MyNav, builder: NavOptionsBuilder.() -> Unit = {}) {
+    navigate(nav.router, builder)
+}
+
+class PetViewModel : ViewModel() {
+    var adoptedList by mutableStateOf(listOf<Int>())
+        private set
+
+    fun adp(pet: Pet) {
+        val adopted = adoptedList.any { a -> a == pet.id }
+        adoptedList = if (adopted) {
+            adoptedList.toMutableList().also { it.remove(pet.id) }
+        } else {
+            adoptedList + listOf(pet.id)
+        }
     }
 }
